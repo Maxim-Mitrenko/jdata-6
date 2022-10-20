7fa6cb07a32578a1020d3cd5da915e559f3e8e06
Maxim-Mitrenko <klevaya.farm@gmail.com>
2022-10-20 23:30:51 +0300

    Add method security

--- src/main/java/com/example/homework1/controller/MyController.java
+++ src/main/java/com/example/homework1/controller/MyController.java
@@ -1,9 +1,12 @@
 package com.example.homework1.controller;
 
+import org.springframework.security.access.annotation.Secured;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+import javax.annotation.security.RolesAllowed;
 import java.util.List;
 import java.util.Random;
 
@@ -14,16 +17,25 @@ public class MyController {
     private int bound = 100;
 
     @GetMapping("/hello")
+    @PreAuthorize("hasAnyRole('random', 'randomList')")
     public String hello() {
         return "Hello";
     }
 
+    @GetMapping("/hi")
+    @PreAuthorize("#username==authentication.principal.username")
+    public String hi(@RequestParam String username) {
+        return "Hi, " + username + "!";
+    }
+
     @GetMapping("/random")
+    @Secured("ROLE_random")
     public String random() {
         return "Ваше счастливое число: " + random.nextInt(bound);
     }
 
     @GetMapping("/randomList")
+    @RolesAllowed("ROLE_randomList")
     public List<Integer> randomList() {
         return random.ints(10, 0, bound)
                 .boxed()
@@ -31,6 +43,7 @@ public class MyController {
     }
 
     @GetMapping("/setBound")
+    @Secured("ROLE_admin")
     public String setBound(@RequestParam int bound) {
         this.bound = bound;
         return "Число " + bound + " успешно установлено в качестве максимального числа для рандома!";
